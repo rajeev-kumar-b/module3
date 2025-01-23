444d3cb1a28dbb1c938c09c8db7a5b52f9497e54
Rajeev Kumar <shardul.bhatlavande@weatherford.com>
2025-01-23 17:16:50 +0530

    module 3 commit

---
+++ app.js
@@ -0,0 +1,77 @@
+(function() {
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com/")
+        .directive('foundItems', FoundItems);
+
+    function FoundItems() {
+        var ddo = {
+            restrict: 'E',
+            templateUrl: 'foundItems.html',
+            scope: {
+                foundItems: '<',
+                onEmpty: '<',
+                onRemove: '&'
+            },
+            controller: NarrowItDownController,
+            controllerAs: 'menu',
+            bindToController: true
+        };
+
+        return ddo;
+    }
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService) {
+        var menu = this;
+        menu.shortName = '';
+
+        menu.matchedMenuItems = function(searchTerm) {
+            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+            promise.then(function(items) {
+                if (items && items.length > 0) {
+                    menu.message = '';
+                    menu.found = items;
+                } else {
+                    menu.message = 'Nothing found!';
+                    menu.found = [];
+                }
+            });
+        };
+
+        menu.removeMenuItem = function(itemIndex) {
+            menu.found.splice(itemIndex, 1);
+        }
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+            return $http({
+                method: "GET",
+                url: (ApiBasePath+"menu_items.json")
+            }).then(function(response) {
+                var foundItems = [];
+
+                Object.entries(response.data).forEach(([key, value]) => { 
+                    Object.entries(value.menu_items).forEach(([key, value]) => {
+                        console.log(`${key}: ${value}`);
+                        if (searchTerm.length > 0 && value.description.toLowerCase().indexOf(searchTerm) !== -1) {
+                                foundItems.push(value);
+                        }
+                    });
+                });
+
+                return foundItems;
+            });
+        };
+    }
+})();
